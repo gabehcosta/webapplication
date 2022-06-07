18d2104130657c5bd0e4738eb6bbb73ac64f294b
Gabriel Costa <g.h.costa@outlook.com>
2022-06-07 23:20:29 +0000

    Adicionando a rota '/cadastro-jogador' no arquivo app.py

--- app.py
+++ app.py
@@ -7,7 +7,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-
+@app.route('/cadastro-jogador')
+def cadastro():
+    return render_template('cadastro-jogador.html')
 
 
 app.run(debug=True)
