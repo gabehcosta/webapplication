c20bac34faf94218bba15ecb92adbbcc05e549d0
Gabriel Costa <g.h.costa@outlook.com>
2022-06-07 23:42:16 +0000

    Passando a lista de jogadores como retorno da função index

--- app.py
+++ app.py
@@ -3,9 +3,15 @@ from uuid import uuid4
 
 app = Flask(__name__)
 
+jogadores = [
+    {'nome':'Neymar JR', 'posicao':'Atacante', 'idade':30, 'clube':'PSG'},
+    {'nome':'Weverton', 'posicao':'Goleiro', 'idade':34, 'clube':'Palmeiras'},
+    {'nome':'Gabriel Jejum', 'posicao':'Atacante', 'idade':25, 'clube':'Manchester City'}
+]
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', jogadores=jogadores)
 
 @app.route('/cadastro-jogador')
 def cadastro():
