0e7982bb034f5a94d78d453c6e64061b89f5f6fa
Gabriel Costa <g.h.costa@outlook.com>
2022-06-13 18:59:00 +0000

    Colocando lógica de ordenação na lista de jogadores

--- app.py
+++ app.py
@@ -4,11 +4,7 @@ from os.path import exists
 import csv
 app = Flask(__name__)
 
-jogadores = [
-    {'id':uuid4(), 'nome':'Neymar JR', 'posicao':'Atacante', 'idade':30, 'clube':'PSG'},
-    {'id':uuid4(), 'nome':'Weverton', 'posicao':'Goleiro', 'idade':34, 'clube':'Palmeiras'},
-    {'id':uuid4(), 'nome':'Gabriel Jejum', 'posicao':'Atacante', 'idade':25, 'clube':'Manchester City'}
-]
+jogadores = []
 
 if not exists('jogadores.csv'):
     with open('jogadores.csv', 'wt') as file_out:
@@ -28,9 +24,11 @@ else:
 def index():
     return render_template('index.html', jogadores=jogadores)
 
+
 @app.route('/cadastro-jogador')
 def cadastro():
     return render_template('cadastro-jogador.html')
+
 
 @app.route('/salvar', methods=['POST'])
 def salvar():
@@ -40,6 +38,9 @@ def salvar():
     clube = request.form['clube']
 
     jogadores.append({'id':uuid4(), 'nome':nome.title(), 'posicao':posicao.title(), 'idade':idade, 'clube':clube.title()})
+    jogadores_ordenados = sorted(jogadores, key=lambda jogador: jogador['posicao'])
+    jogadores = jogadores_ordenados
+
     with open('jogadores.csv', 'wt') as file_out:
         escritor = csv.DictWriter(file_out, ['id', 'nome', 'posicao', 'idade', 'clube']) 
         escritor.writeheader()
@@ -47,13 +48,20 @@ def salvar():
     
     return redirect(url_for('index'))
 
+
 @app.route('/excluir/<id>')
 def excluir(id):
     for jogador in jogadores:
         if str(jogador['id']) == str(id):
             jogadores.remove(jogador)
+    
+    with open('jogadores.csv', 'wt') as file_out:
+        escritor = csv.DictWriter(file_out, ['id', 'nome', 'posicao', 'idade', 'clube']) 
+        escritor.writeheader()
+        escritor.writerows(jogadores)
 
     return render_template('index.html', jogadores=jogadores)
+
 
 @app.route('/editar/<id>')
 def editar(id):
@@ -62,8 +70,7 @@ def editar(id):
             jogador_escolhido = jogador
 
     return render_template('edicao-jogador.html', jogador=jogador_escolhido)
-    
-    
+
 
 @app.route('/salvar-edicao', methods=['POST'])
 def salvar_edicao():
@@ -72,22 +79,29 @@ def salvar_edicao():
     posicao_editado = request.form['posicao_edicao']
     idade_editado = request.form['idade_edicao']
     clube_editado = request.form['clube_edicao']
+    posicao_alterada = False
+
+    if posicao_editado != jogador['posicao']:
+        posicao_alterada = True
 
     for jogador in jogadores:
         if str(jogador['id']) == str(id_editado):
             jogador['nome'] = nome_editado.title()
             jogador['posicao'] = posicao_editado.title()
+    
             jogador['idade'] = idade_editado
             jogador['clube'] = clube_editado.title()
 
+    if posicao_alterada:
+        jogadores_ordenados = sorted(jogadores, key=lambda jogador: jogador['posicao'])
+        jogadores = jogadores_ordenados
+    
     with open('jogadores.csv', 'wt') as file_out:
         escritor = csv.DictWriter(file_out, ['id', 'nome', 'posicao', 'idade', 'clube']) 
         escritor.writeheader()
         escritor.writerows(jogadores)
 
     return redirect(url_for('index'))
-
-
 
 
 app.run(debug=True)
