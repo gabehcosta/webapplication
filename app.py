fb509fe8172de0e0ae6318c3e305462c987f7c5a
Gabriel Costa <g.h.costa@outlook.com>
2022-06-13 13:20:11 +0000

    Adicionando rotas de atualização do formulário em app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def index():
 def cadastro():
     return render_template('cadastro-jogador.html')
 
-@app.route('/salvar', methods=['post'])
+@app.route('/salvar', methods=['POST'])
 def salvar():
     nome = request.form['nome']
     posicao = request.form['posicao']
@@ -27,6 +27,42 @@ def salvar():
     jogadores.append({'id':uuid4(), 'nome':nome, 'posicao':posicao, 'idade':idade, 'clube':clube})
     return redirect(url_for('index'))
 
-# @app.route('')
+@app.route('/excluir/<id>')
+def excluir(id):
+    for jogador in jogadores:
+        if str(jogador['id']) == str(id):
+            jogadores.remove(jogador)
+
+    return render_template('index.html', jogadores=jogadores)
+
+@app.route('/editar/<id>')
+def editar(id):
+    for jogador in jogadores:
+        if str(jogador['id']) == str(id):
+            jogador_escolhido = jogador
+
+    return render_template('edicao-jogador.html', jogador=jogador_escolhido)
+    
+    
+
+@app.route('/salvar-edicao', methods=['POST'])
+def salvar_edicao():
+    id_editado = request.form['id_edicao']
+    nome_editado = request.form['nome_edicao']
+    posicao_editado = request.form['posicao_edicao']
+    idade_editado = request.form['idade_edicao']
+    clube_editado = request.form['clube_edicao']
+
+    for jogador in jogadores:
+        if str(jogador['id']) == str(id_editado):
+            jogador['nome'] = nome_editado
+            jogador['posicao'] = posicao_editado
+            jogador['idade'] = idade_editado
+            jogador['clube'] = clube_editado
+
+    return redirect(url_for('index'))
+
+
+
 
 app.run(debug=True)
